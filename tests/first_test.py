99d03ecd54828cf9747a09260a6f43b902f1b088
Kirill Sheregov <112244472+LlirikSh@users.noreply.github.com>
2022-10-08 16:31:26 +0300

    Update first_test.py

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,16 +1,6 @@
 import pytest
 from app.calculator import Calculator
 
-# class TestCalc:
-#     def setup(self):
-#         self.calc=Calculator
-#
-#     def test_myltiply_calculate_correctly(self):
-#         assert self.calc.multiply(self, 2, 2) == 4
-#
-#     def test_myltiply_calculation_failed(self):
-#         assert self.calc.multiply(self, 2, 2) == 5
-
 class TestCalc:
    def setup(self):
        self.calc = Calculator
